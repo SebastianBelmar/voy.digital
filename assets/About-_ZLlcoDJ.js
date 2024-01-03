e28c91f897a48db1bfb12958e46847271c002fc3
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-03 16:13:59 -0300

    Updates

---
+++ assets/About-_ZLlcoDJ.js
@@ -0,0 +1,1 @@
+import{f as t,o,c as e}from"./index-F3mpTkmE.js";const s={},c={class:"font-unbounded text-4xl"};function n(r,a,_,f,p,u){return o(),e("h1",c,"About")}const l=t(s,[["render",n]]);export{l as default};
---
+++ assets/HomeLayout-pcm1IGpg.js
@@ -0,0 +1,7 @@
+import{r as p,e as v,o as d,c as m,a as o,u as t,b as E,F as f,d as n,_ as a}from"./index-F3mpTkmE.js";const B={class:"w-1/2"},P={__name:"HomeLayout",setup(V){const r=n(()=>a(()=>import("./NavBar-pwpKSaBx.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),_=n(()=>a(()=>import("./SideBar-zUCCAdW_.js"),__vite__mapDeps([7,1,2,4,5,8,3,9]))),s=n(()=>a(()=>import("./Footer-JqeUSKui.js"),__vite__mapDeps([10,4,1,2,8]))),c=n(()=>a(()=>import("./FloatingButton-RkpqwGOb.js"),__vite__mapDeps([11,1,2,12]))),e=p(!1),l=()=>{e.value=!e.value},u=()=>{e.value=!1};return(A,L)=>{const i=v("router-view");return d(),m(f,null,[o(t(_),{onCloseEvent:u,open:e.value},null,8,["open"]),o(t(r),{onOpenEvent:l,open:e.value},null,8,["open"]),o(i),o(t(s)),E("div",B,[o(t(c))])],64)}}};export{P as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/NavBar-pwpKSaBx.js","assets/index-F3mpTkmE.js","assets/index-uKNHXMS2.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-dfg6bjFt.js","assets/ButtonMeeting-lBkrbf2u.js","assets/NavBar-vOmcWfZY.css","assets/SideBar-zUCCAdW_.js","assets/SocialNetworks-gAElPQ6A.js","assets/SideBar-iTX83jdv.css","assets/Footer-JqeUSKui.js","assets/FloatingButton-RkpqwGOb.js","assets/FloatingButton-kPmiSjmM.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
