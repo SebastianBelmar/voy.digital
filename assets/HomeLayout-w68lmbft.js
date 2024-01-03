0d22a278a43aecfb6828ead70c611f89a5da2cd9
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-03 16:26:30 -0300

    Updates

---
+++ assets/HomeLayout-w68lmbft.js
@@ -0,0 +1,7 @@
+import{r as p,e as v,o as d,c as m,a as o,u as t,b as E,F as f,d as n,_ as a}from"./index-rsFG1pXF.js";const B={class:"w-1/2"},P={__name:"HomeLayout",setup(V){const r=n(()=>a(()=>import("./NavBar-XSyrSgPy.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),_=n(()=>a(()=>import("./SideBar-fuYYmJgj.js"),__vite__mapDeps([7,1,2,4,5,8,3,9]))),s=n(()=>a(()=>import("./Footer-pB89_EXO.js"),__vite__mapDeps([10,4,1,2,8]))),c=n(()=>a(()=>import("./FloatingButton-Bl7MSAWT.js"),__vite__mapDeps([11,1,2,12]))),e=p(!1),l=()=>{e.value=!e.value},u=()=>{e.value=!1};return(A,L)=>{const i=v("router-view");return d(),m(f,null,[o(t(_),{onCloseEvent:u,open:e.value},null,8,["open"]),o(t(r),{onOpenEvent:l,open:e.value},null,8,["open"]),o(i),o(t(s)),E("div",B,[o(t(c))])],64)}}};export{P as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/NavBar-XSyrSgPy.js","assets/index-rsFG1pXF.js","assets/index-uKNHXMS2.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-RhE1EGLb.js","assets/ButtonMeeting-AwVLUu1E.js","assets/NavBar-vOmcWfZY.css","assets/SideBar-fuYYmJgj.js","assets/SocialNetworks-aRjAx3eC.js","assets/SideBar-iTX83jdv.css","assets/Footer-pB89_EXO.js","assets/FloatingButton-Bl7MSAWT.js","assets/FloatingButton-kPmiSjmM.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
