a4e1f28b665073391dea4063eacf76dbe84d3759
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-03 05:55:15 -0300

    Updates

---
+++ assets/Logo-2LRJTR0s.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,x as t}from"./index-YOu0IZSB.js";const c="/voy.digital/assets/logo-w4Cz797l.svg",m={__name:"Logo",setup(a){const o=()=>{t().push({name:"welcome",params:{}})};return(s,n)=>(e(),r("img",{class:"cursor-pointer",src:c,alt:"logo",onClick:o}))}};export{m as _};
---
+++ assets/NotFound-N38xxCyG.js
@@ -0,0 +1,7 @@
+import{o as a,c as o,a as s,u as n,F as r,b as e,d as c,_}from"./index-YOu0IZSB.js";const l=e("div",{class:"w-full text-center p-[20vh]"},[e("h1",{class:"text-oscuro text-4xl"},"Página No Encontrada")],-1),m={__name:"NotFound",setup(d){const t=c(()=>_(()=>import("./NavBar-iLSFE44a.js"),__vite__mapDeps([0,1,2,3,4,5,6])));return(u,i)=>(a(),o(r,null,[s(n(t)),l],64))}};export{m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/NavBar-iLSFE44a.js","assets/index-YOu0IZSB.js","assets/index-uKNHXMS2.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-2LRJTR0s.js","assets/ButtonMeeting-UsZngIJa.js","assets/NavBar-vOmcWfZY.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
