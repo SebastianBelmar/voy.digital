b37bfd26d592449e798430f884cffabccbbc25c5
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-03 15:28:29 -0300

    Updates

--- assets/About-00tDGSRf.js
+++
@@ -1,1 +0,0 @@
-import{f as t,o,c as e}from"./index-YlO2yvps.js";const s={},c={class:"font-unbounded text-4xl"};function n(r,a,_,f,p,u){return o(),e("h1",c,"About")}const l=t(s,[["render",n]]);export{l as default};
