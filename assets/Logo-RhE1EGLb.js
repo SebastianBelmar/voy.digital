a3c741c987482b29adeb80fcf637543fa9f06e77
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-03 18:36:01 -0300

    Updates

--- assets/Logo-RhE1EGLb.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as r,x as t}from"./index-rsFG1pXF.js";const c="/assets/logo-w4Cz797l.svg",m={__name:"Logo",setup(a){const o=()=>{t().push({name:"welcome",params:{}})};return(s,n)=>(e(),r("img",{class:"cursor-pointer",src:c,alt:"logo",onClick:o}))}};export{m as _};
