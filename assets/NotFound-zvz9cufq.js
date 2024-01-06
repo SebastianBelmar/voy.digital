b27f02a153e94caabdc77eec8be5822891ce9668
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-06 04:17:58 -0300

    Updates

---
+++ assets/NotFound-zvz9cufq.js
@@ -0,0 +1,7 @@
+import{o as a,c as o,a as s,u as n,F as r,b as e,d as c,_}from"./index-Mn-iFcsC.js";const l=e("div",{class:"w-full text-center p-[20vh]"},[e("h1",{class:"text-oscuro text-4xl"},"Página No Encontrada")],-1),m={__name:"NotFound",setup(d){const t=c(()=>_(()=>import("./NavBar-wnzviuBq.js"),__vite__mapDeps([0,1,2,3,4,5,6])));return(u,i)=>(a(),o(r,null,[s(n(t)),l],64))}};export{m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/NavBar-wnzviuBq.js","assets/index-Mn-iFcsC.js","assets/index-DgTTUOrM.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-j03dtKKB.js","assets/ButtonMeeting-o2UsYnUC.js","assets/NavBar-NtoSBr0M.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
