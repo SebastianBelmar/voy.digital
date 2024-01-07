dedd7a764c75ca615df3f05c3548fc33e34367a8
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-07 04:32:06 -0300

    Updates

--- assets/NotFound-c-DILyNg.js
+++
@@ -1,7 +0,0 @@
-import{o as a,c as o,a as s,u as n,F as r,b as e,d as c,_}from"./index--NY8ksvl.js";const l=e("div",{class:"w-full text-center p-[20vh]"},[e("h1",{class:"text-oscuro text-4xl"},"Página No Encontrada")],-1),m={__name:"NotFound",setup(d){const t=c(()=>_(()=>import("./NavBar-WDnKY1Ck.js"),__vite__mapDeps([0,1,2,3,4,5,6])));return(u,i)=>(a(),o(r,null,[s(n(t)),l],64))}};export{m as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/NavBar-WDnKY1Ck.js","assets/index--NY8ksvl.js","assets/index-DgTTUOrM.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-zGeC0AXQ.js","assets/ButtonMeeting-jTIi-lGL.js","assets/NavBar-NtoSBr0M.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
