6ca9cc3a220d71520b2d6d621e79bc4046f54410
Sebastian <sebastian.belmar@ufrontera.cl>
2024-01-13 22:26:10 -0300

    Updates

--- assets/HomeLayout-GjjI0nvE.js
+++
@@ -1,7 +0,0 @@
-import{r as p,e as v,o as d,c as m,a as o,u as t,b as E,F as f,d as n,_ as a}from"./index-S7SqTadR.js";const B={class:"w-1/2"},P={__name:"HomeLayout",setup(V){const r=n(()=>a(()=>import("./NavBar-Fv0ZUkvU.js"),__vite__mapDeps([0,1,2,3,4,5,6]))),_=n(()=>a(()=>import("./SideBar-Dn7Rd2nW.js"),__vite__mapDeps([7,1,2,4,5,8,3,9]))),s=n(()=>a(()=>import("./Footer-9I0JHRhm.js"),__vite__mapDeps([10,4,1,2,8]))),c=n(()=>a(()=>import("./FloatingButton-qs8PQ-xj.js"),__vite__mapDeps([11,1,2,12]))),e=p(!1),l=()=>{e.value=!e.value},u=()=>{e.value=!1};return(A,L)=>{const i=v("router-view");return d(),m(f,null,[o(t(_),{onCloseEvent:u,open:e.value},null,8,["open"]),o(t(r),{onOpenEvent:l,open:e.value},null,8,["open"]),o(i),o(t(s)),E("div",B,[o(t(c))])],64)}}};export{P as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/NavBar-Fv0ZUkvU.js","assets/index-S7SqTadR.js","assets/index-n_1Nsml-.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-L9bBU4bz.js","assets/ButtonMeeting-5fAB4nXT.js","assets/NavBar-NtoSBr0M.css","assets/SideBar-Dn7Rd2nW.js","assets/SocialNetworks-JdrG-DzO.js","assets/SideBar-dB3kll3v.css","assets/Footer-9I0JHRhm.js","assets/FloatingButton-qs8PQ-xj.js","assets/FloatingButton-Xv5hW2Az.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
--- assets/NotFound-rbXTBS_p.js
+++
@@ -1,7 +0,0 @@
-import{o as a,c as o,a as s,u as n,F as r,b as e,d as c,_}from"./index-S7SqTadR.js";const l=e("div",{class:"w-full text-center p-[20vh]"},[e("h1",{class:"text-oscuro text-4xl"},"Página No Encontrada")],-1),m={__name:"NotFound",setup(d){const t=c(()=>_(()=>import("./NavBar-Fv0ZUkvU.js"),__vite__mapDeps([0,1,2,3,4,5,6])));return(u,i)=>(a(),o(r,null,[s(n(t)),l],64))}};export{m as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/NavBar-Fv0ZUkvU.js","assets/index-S7SqTadR.js","assets/index-n_1Nsml-.css","assets/scrollMenu-N3kOyiLQ.js","assets/Logo-L9bBU4bz.js","assets/ButtonMeeting-5fAB4nXT.js","assets/NavBar-NtoSBr0M.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
